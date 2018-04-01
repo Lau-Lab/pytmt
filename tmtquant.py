4d63eee96026221f9682c5d8cb78c5b11f86d1da
Edward Lau <edward.lau@me.com>
2018-03-31 23:32:01 -0600

    first working version

--- tmtquant.py
+++ tmtquant.py
@@ -1,129 +1,157 @@
-import pymzml as mz
+"""
+TMT quantifier v.0.1.0
+Reads in Crux/Percolator tab-delimited results (PSMS) and returns TMT values
+
+Currently supports only Percolator, and MS2 quant.
+To do: TMT 6-plex, MS3 or multi-notch, etc.
+
+Molecular Proteomics Laboratory
+http://maggielab.org
+
+"""
+
+import pymzml
 import os.path
-
 import re
-
-#import os.path
 import pandas as pd
-#import scipy.integrate
-from time import time
+import xml
 
+# Folders of mzML files and the Percolator results.
+mzml_loc = './test_mzml_2'
+id_loc = './test_perc_2'
 
-class Mzml(object):
-    """
-    This class handles the Mzml files to be read
-    """
+# Define the reporter ion m/z values. Support TMT-10plex for now.
+reporters = [126.127726,
+             127.124761,
+             127.131081,
+             128.128116,
+             128.134436,
+             129.131471,
+             129.137790,
+             130.134825,
+             130.141145,
+             131.138180
+             ]
 
-    def __init__(self, path):
-        """
+# Define the PPM of integration
+precision = 20
 
-        :param path:
-        """
+# Define the Percolator Q value cutoff filter
+qFilter = 0.05
 
-        self.path = os.path.join(path)
-        self.msdata = mz.run.Reader(self.path, MS1_Precision=20e-6, MSn_Precision=20e-6)
+# Define the Percolator protein-unique PSM filter
+uniqueOnly = True
 
-    def get_reporter_from_scan_id(self, reporter_list, spectrum_id):
-        """
+assert os.path.isdir(mzml_loc), 'Percolator directory not valid.'
+assert os.path.isdir(id_loc), 'Percolator directory not valid.'
 
-        :param reporter_list:
-        :param spectrum_id:
-        :return:
-        """
-        pass
+# List all files in the percolator directory ending with target.psms.txt.
+id_files = [f for f in os.listdir(id_loc) if f.endswith('target.psms.txt')]
 
+assert len(id_files) == 1, 'Check percolator output directory has 1 *.target.psms.txt'
 
+# Read the Percolator psms file.
+id_df = pd.read_csv(filepath_or_buffer=os.path.join(id_loc, id_files[0]),
+                    sep='\t')
 
+# Get all the file indices in the Percolator results file.
+file_indices = list(set(id_df['file_idx']))
 
-mzml_loc = "./test_mzml_1"
-perc_loc = "./test_perc_1"
-
-assert os.path.isdir(perc_loc)
-
-# List all files in the percolator directory ending with target.psms.txt
-perc_psms_files = [f for f in os.listdir(perc_loc) if f.endswith('target.psms.txt')]
-
-assert len(perc_psms_files) == 1, 'Check percolator output directory has 1 target.psms.txt'
-
-# Read the psms file.
-psms_df = pd.read_csv(filepath_or_buffer=os.path.join(perc_loc, perc_psms_files[0]),
-                      sep="\t")
-
-# Get all the file indices in the run
-file_indices = list(set(psms_df['file_idx']))
-
-# Check that the number of mzMLs in the mzml folder is the same as the maximum of the ID's file_idx column
-# Note this will throw an error if not every fraction results in at least some ID, but we are going to
-# ignore this for now
+# Check that the number of mzMLs in the mzML folder is the same as the maximum of the ID file's file_idx column.
+# Note this will throw an error if not every fraction results in at least some ID, but we will ignore for now.
 
 mzml_files = [f for f in os.listdir(mzml_loc) if re.match('^.*.mzML', f)]
-# Sort by names
+
+# Sort the mzML files by names
+# Note this may create a problem if the OS Percolator runs on has natural sorting (xxxx_2 before xxxx_10)
+# But we will ignore for now
 mzml_files.sort()
 
 # Throw an error if there is no mzML file in the mzml directory
-if len(mzml_files) == 0:
-    try:
-        raise Exception('No mzML files in the specified directory')
-    except ValueError as e:
-        print('Unknown error')
+assert len(mzml_files) != 0, 'No mzML files in the specified directory.'
+assert len(mzml_files) == max(file_indices) + 1, 'Number of files not matching.'
 
-assert len(mzml_files) == max(file_indices) + 1, 'Number of files not matching'
 
-# Loop through each file index, then
-
+# For each file index (fraction), open the mzML file, and create a subset Percolator ID dataframe
 for idx in file_indices:
 
-    # Make a sub_df with current index being considered
-    sub_df = psms_df[psms_df['file_idx'] == idx]
+    print('Doing mzML:', mzml_files[idx], '(' + str(idx + 1), 'of', str(len(file_indices)) + ')', sep= ' ')
 
-    # Arrange by scan number
-    sub_df = sub_df.sort_values(by='scan')
+    # Make a subset dataframe with the current file index (fraction) being considered
+    fraction_id_df = id_df[id_df['file_idx'] == idx]
 
-    # Filter by q value
-    sub_df_filter = sub_df[sub_df['percolator q-value'] <= 0.05]
-    sub_df_filter = sub_df_filter.reset_index()
+    # Arrange the PSM rows by scan number
+    fraction_id_df = fraction_id_df.sort_values(by='scan').reset_index(drop=True)
 
     # Open the mzML file
-    msdata = mz.run.Reader(path=os.path.join(mzml_loc, mzml_files[idx]),
-                  MS1_Precision=20e-6, MSn_Precision=20e-6)
-
+    fraction_mzml = pymzml.run.Reader(path=os.path.join(mzml_loc, mzml_files[idx]),
+                                  MS1_Precision=precision*1e-6, MSn_Precision=precision*1e-6)
 
     # Loop through each qualifying row in sub_df_filtered
+    for i in range(len(fraction_id_df)):
 
-    for i in range(len(sub_df_filter)):
+        # If this is a qualifying PSM, pull the spectrum based on the scan number
+        scan = fraction_id_df.loc[i, 'scan']
 
-        # scan number
-        scan_num = sub_df_filter.ix[i, 'scan']
+        if (i+1) % 100 == 0:
+            print('Doing mzML:', mzml_files[idx], '(' + str(idx + 1), 'of', str(len(file_indices)) + ');',
+                  'PSM:', i+1, 'of', len(fraction_id_df), '(scan number:', str(scan) + ')', sep=' ')
 
-        # Get the spectrum based on the spectrum number
+        # If q-value filter is on, skip any row that fails the filter.
+        if fraction_id_df.loc[i, 'percolator q-value'] > qFilter:
+            continue
+
+        # If the protein-unique PSM filter is on, skip any row that fails the filter.
+        if uniqueOnly and len(fraction_id_df.loc[i, 'protein id'].split(',')) > 1:
+            continue
+
+        # Get the spectrum in the mzML file by scan number
         try:
-            spectrum = msdata[scan_num]
+            spectrum = fraction_mzml[scan]
 
         except KeyError:
-            print("Spectrum not found")
+            print('Spectrum index out of bound.')
 
-        tmt_list = [126.127726,
-                    127.124761,
-                    127.131081,
-                    128.128116,
-                    128.134436,
-                    129.131471,
-                    129.137790,
-                    130.134825,
-                    130.141145,
-                    131.138180
-                    ]
+        except xml.etree.ElementTree.ParseError:
+            print('XML eTree does not appear to be able to read this spectrum')
+            continue
 
-        tmt_intensities = []
+        assert spectrum['ms level'] > 1, 'Spectrum is not MSn.'
 
-        for reporter in tmt_list:
+        # For each reporter, check that the spectrum has that peak using pymzml
+        # This returns a m/z and intensity tuple
+        # We will append the intensity of each reporter to a list
+        tmt_intensities = [idx, scan]
 
+        for reporter in reporters:
             matchList = spectrum.has_peak(reporter)
 
-            if matchList:
+            if matchList and len(matchList) == 1:
                 for mz, I in matchList:
-                    tmt_intensities.append([scan_num, i, I, mz])
+                    tmt_intensities.append(I)
             else:
-                tmt_intensities.append([scan_num, i, 0, 0])
+                tmt_intensities.append(0)
+
+        # Grow the results into an output list of lists (creating if does not exist)
+        try:
+            output_list.append(tmt_intensities)
+        except NameError:
+            output_list = [tmt_intensities]
+
+# Turn the output list into a data frame
+output_df_columns = ['file_idx', 'scan', ]
+
+for reporter in reporters:
+    output_df_columns.append('m' + str(reporter))
+
+output_df = pd.DataFrame(output_list, columns=output_df_columns)
 
 
+
+# Final output, merging the input and output tables
+
+final_df = pd.merge(id_df, output_df, how='left')
+
+
+
+final_df.to_csv('output.csv')
