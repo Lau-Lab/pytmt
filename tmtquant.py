90f49df83f20a53060aebd8357f2aac64b708950
Edward Lau <edward.lau@me.com>
2019-09-06 15:58:33 -0600

    added 0, 2, 6, 10, 11, 16 plex reporters

--- tmtquant.py
+++ tmtquant.py
@@ -125,69 +125,40 @@ def quant(args):
     assert args.multiplex in [0, 2, 6, 10, 11, 16], '[error] TMT multiplexity not 0, 2, 6, 10, 11 or 16'
 
     # Define the reporter ion m/z values. Support TMT 0, 2, 6, 10, 11, 16-plex for now.
-    if args.multiplex == 10:
-        reporters = [126.127726,
-                     127.124761,
-                     127.131081,
-                     128.128116,
-                     128.134436,
-                     129.131471,
-                     129.137790,
-                     130.134825,
-                     130.141145,
-                     131.138180,
+    all_reporters = [126.127726,  # 126
+                     127.124761,  # 127N
+                     127.131081,  # 127C
+                     128.128116,  # 128N
+                     128.134436,  # 128C
+                     129.131471,  # 129N
+                     129.137790,  # 129C
+                     130.134825,  # 130N
+                     130.141145,  # 130C
+                     131.138180,  # 131N
+                     131.144499,  # 131C (11-plex)
+                     132.141535,  # 132N (Pro)
+                     132.147855,  # 132C (Pro)
+                     133.144890,  # 133N (Pro)
+                     133.141210,  # 133C (Pro)
+                     134.148245,  # 134C (Pro)
                      ]
+    if args.multiplex == 16:
+        reporters = [i for n, i in enumerate(all_reporters)]
 
     elif args.multiplex == 11:
-        reporters = [126.127726,
-                     127.124761,
-                     127.131081,
-                     128.128116,
-                     128.134436,
-                     129.131471,
-                     129.137790,
-                     130.134825,
-                     130.141145,
-                     131.138180,
-                     131.144499,
-                     ]
+        reporters = [i for n, i in enumerate(all_reporters) if n in range(0, 11)]
 
-    elif args.multiplex == 16:
-        reporters = [126.127726,
-                     127.124761,
-                     127.131081,
-                     128.128116,
-                     128.134436,
-                     129.131471,
-                     129.137790,
-                     130.134825,
-                     130.141145,
-                     131.138180,
-                     131.144499,
-                     132.141535,
-                     132.147855,
-                     133.144890,
-                     133.141210,
-                     134.148245,
-                     ]
-
-    elif args.multiplex == 0:
-        reporters = [126.127726,
-                     ]
-
-    elif args.multiplex == 2:
-        reporters = [126.127726,
-                     127.131081,
-                     ]
+    elif args.multiplex == 10:
+        reporters = [i for n, i in enumerate(all_reporters) if n in range(0, 10)]
 
     elif args.multiplex == 6:
-        reporters = [126.127726,
-                     127.131081,
-                     128.134436,
-                     129.131471,
-                     130.141145,
-                     131.138180,
-                     ]
+        reporters = [i for n, i in enumerate(all_reporters) if n in [0, 2, 4, 5, 8, 9]]
+
+    elif args.multiplex == 2:
+        reporters = [i for n, i in enumerate(all_reporters) if n in [0, 2]]
+
+    elif args.multiplex == 0:
+        reporters = [i for n, i in enumerate(all_reporters) if n in range(0, 1)]
 
     # Define the PPM of integration
     precision = args.precision
